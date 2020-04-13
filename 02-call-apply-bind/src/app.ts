113b91826ba7f33d948f6ffa08188fd659235c16
danielmarbach <daniel.marbach@openplace.net>
2020-04-13 22:29:56 +0200

    Comments on bind and call

--- 02-call-apply-bind/src/app.ts
+++ 02-call-apply-bind/src/app.ts
@@ -10,9 +10,12 @@ const myObj = {
 function myFunction(...text: string[]) {
   console.log('Function:::', this, text);
 }
+// binds this to myObject
 const bindFunction = myFunction.bind(myObj);
 bindFunction('ABC', 'DEF');
 bindFunction('123', '456');
 bindFunction('ABC', 'DEF');
+// calls the function with this set to myObj and passes these comma seperated args into it
 myFunction.call(myObj, 'ABC', 'DEF');
+// does the same as above but passes an array along
 myFunction.apply(myObj, ['ABC', 'DEF']);
